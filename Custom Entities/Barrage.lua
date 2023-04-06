b66b81f6f35479b530e963937987a179d9f48a3b
Ice <67441866+MrNooberDude@users.noreply.github.com>
2023-04-06 09:37:23 -0400

    Made Barrage.lua good

--- Custom Entities/Barrage.lua
+++ Custom Entities/Barrage.lua
@@ -1,1 +1,92 @@
+local Spawner = loadstring(game:HttpGet("https://raw.githubusercontent.com/RegularVynixu/Utilities/main/Doors%20Entity%20Spawner/Source.lua"))()
 
+--9117118928
+--9112775175
+
+-- Create entity
+local entityTable = Spawner.createEntity({
+    CustomName = "Barrage", -- Custom name of your entity
+    Model = "https://github.com/MrNooberDude/Doors-Custom-Things/blob/main/Custom%20Entities/Barrage.rbxm?raw=true", -- Can be GitHub file or rbxassetid
+    Speed = 125, -- Percentage, 100 = default Rush speed
+    DelayTime = 0, -- Time before starting cycles (seconds)
+    HeightOffset = 0,
+    CanKill = true,
+    KillRange = 50,
+    BackwardsMovement = false,
+    BreakLights = true,
+    FlickerLights = {
+        true, -- Enabled/Disabled
+        2, -- Time (seconds)
+    },
+    Cycles = {
+        Min = 3,
+        Max = 7,
+        WaitTime = 0,
+    },
+    CamShake = {
+        true, -- Enabled/Disabled
+        {3.5, 20, 0.1, 1}, -- Shake values (don't change if you don't know)
+        100, -- Shake start distance (from Entity to you)
+    },
+    Jumpscare = {
+        true, -- Enabled/Disabled
+        {
+            Image1 = "rbxassetid://13017861542", -- Image1 url
+            Image2 = "rbxassetid://13017861542", -- Image2 url
+            Shake = true,
+            Sound1 = {
+                3359047385, -- SoundId
+                { Volume = 0.5, PlaybackSpeed = 0.5 }, -- Sound properties
+            },
+            Sound2 = {
+                3359047385, -- SoundId
+                { Volume = 1.5 }, -- Sound properties
+            },
+            Flashing = {
+                true, -- Enabled/Disabled
+                Color3.fromRGB(255, 255, 255), -- Color
+            },
+            Tease = {
+                true, -- Enabled/Disabled
+                Min = 0,
+                Max = 1,
+            },
+        },
+    },
+    CustomDialog = {"Barrage is a more difficult version of Ambush.", "Make sure to pay attention to it's scream! That will give you it's location!"}, -- Custom death message
+})
+
+
+-----[[  Debug -=- Advanced  ]]-----
+entityTable.Debug.OnEntitySpawned = function()
+    print("Entity has spawned:", entityTable)
+end
+
+entityTable.Debug.OnEntityDespawned = function()
+    print("Entity has despawned:", entityTable)
+end
+
+entityTable.Debug.OnEntityStartMoving = function()
+    print("Entity has started moving:", entityTable)
+end
+
+entityTable.Debug.OnEntityFinishedRebound = function()
+    print("Entity has finished rebound:", entityTable)
+end
+
+entityTable.Debug.OnEntityEnteredRoom = function(room)
+    print("Entity:", entityTable, "has entered room:", room)
+end
+
+entityTable.Debug.OnLookAtEntity = function()
+    print("Player has looked at entity:", entityTable)
+end
+
+entityTable.Debug.OnDeath = function()
+    warn("Player has died.")
+end
+------------------------------------
+
+
+-- Run the created entity
+Spawner.runEntity(entityTable)
