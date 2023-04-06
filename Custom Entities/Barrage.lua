8c6c6a4e186d740295dc13aa9ff89210a1da4afa
Ice <67441866+MrNooberDude@users.noreply.github.com>
2023-04-06 17:55:53 -0400

    Update Barrage.lua

--- Custom Entities/Barrage.lua
+++ Custom Entities/Barrage.lua
@@ -58,34 +58,32 @@ local entityTable = Spawner.createEntity({
 
 
 -----[[  Debug -=- Advanced  ]]-----
-if entityTable.Debug ~= nil then
-    entityTable.Debug.OnEntitySpawned = function()
-        print("Entity has spawned:", entityTable)
-    end
+entityTable.Debug.OnEntitySpawned = function()
+    print("Entity has spawned:", entityTable)
+end
 
-    entityTable.Debug.OnEntityDespawned = function()
-        print("Entity has despawned:", entityTable)
-    end
+entityTable.Debug.OnEntityDespawned = function()
+    print("Entity has despawned:", entityTable)
+end
 
-    entityTable.Debug.OnEntityStartMoving = function()
-        print("Entity has started moving:", entityTable)
-    end
+entityTable.Debug.OnEntityStartMoving = function()
+    print("Entity has started moving:", entityTable)
+end
 
-    entityTable.Debug.OnEntityFinishedRebound = function()
-        print("Entity has finished rebound:", entityTable)
-    end
+entityTable.Debug.OnEntityFinishedRebound = function()
+    print("Entity has finished rebound:", entityTable)
+end
 
-    entityTable.Debug.OnEntityEnteredRoom = function(room)
-        print("Entity:", entityTable, "has entered room:", room)
-    end
+entityTable.Debug.OnEntityEnteredRoom = function(room)
+    print("Entity:", entityTable, "has entered room:", room)
+end
 
-    entityTable.Debug.OnLookAtEntity = function()
-        print("Player has looked at entity:", entityTable)
-    end
+entityTable.Debug.OnLookAtEntity = function()
+    print("Player has looked at entity:", entityTable)
+end
 
-    entityTable.Debug.OnDeath = function()
-        warn("Player has died.")
-    end
+entityTable.Debug.OnDeath = function()
+    warn("Player has died.")
 end
 ------------------------------------
 
